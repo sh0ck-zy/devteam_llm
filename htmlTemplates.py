df2c5f259c72a8ebc3b87f80b0be59e541591374
sh0ck-zy <81766337+sh0ck-zy@users.noreply.github.com>
2024-01-29 11:09:55 +0100

    initial commit
    base code for langchain & streamlit llm app

---
+++ htmlTemplates.py
@@ -0,0 +1,44 @@
+css = '''
+<style>
+.chat-message {
+    padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
+}
+.chat-message.user {
+    background-color: #2b313e
+}
+.chat-message.bot {
+    background-color: #475063
+}
+.chat-message .avatar {
+  width: 20%;
+}
+.chat-message .avatar img {
+  max-width: 78px;
+  max-height: 78px;
+  border-radius: 50%;
+  object-fit: cover;
+}
+.chat-message .message {
+  width: 80%;
+  padding: 0 1.5rem;
+  color: #fff;
+}
+'''
+
+bot_template = '''
+<div class="chat-message bot">
+    <div class="avatar">
+        <img src="3f7b9bf8-0108-4181-bdfd-b4110c8cb75f.webp" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
+    </div>
+    <div class="message">{{MSG}}</div>
+</div>
+'''
+
+user_template = '''
+<div class="chat-message user">
+    <div class="avatar">
+        <img src="ae979ef0-cf31-4fbd-878d-b56b405fe120.webp">
+    </div>    
+    <div class="message">{{MSG}}</div>
+</div>
+'''
